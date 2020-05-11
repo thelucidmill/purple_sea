11bd72d0ecb8153e2f105d76a7a5caacf1e4f532
thelucidmill <thelucidmill@mail.com>
2020-05-11 21:44:41 +0100

    home page functionality

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,11 +1,20 @@
-import 'css/site.scss'
+// import 'css/site.scss'
 
-$(document).ready(function(){
-  $("p").click(function(){
-    $(this).hide();
-  });
-});
-function myFunction() {
-  alert("Page is loaded");
-}
-export default myFunction;
+// $(document).ready(function(){
+//   $("p").click(function(){
+//     $(this).hide();
+//   });
+// });
+// function myFunction() {
+//   alert("Page is loaded");
+// }
+// export default myFunction;
+
+
+// :javascript
+//   $(document).ready(function(){
+//     $("#home img").click(function(){
+//       alert('ashdjbh');
+//       $('.landing').hide();
+//     });
+//   });
