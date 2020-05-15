74f63e23b7c1b13c2ef2c74623fd8d75a2cd2031
thelucidmill <thelucidmill@mail.com>
2020-05-15 17:01:53 +0100

    style contact page

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -19,28 +19,72 @@
 //     });
 //   });
 
-:javascript
-      $(document).ready(function(){
-        $(".landing img").click(function(){
-          $( ".landing" ).hide();
-          $( ".ucd" ).show();
-        });
-        $(".ucd img").click(function(){
-          $( ".ucd" ).hide();
-          $( ".ecommerce" ).show();
-        });
-        $(".ecommerce img").click(function(){
-          $( ".ecommerce" ).hide();
-          $( ".business-systems" ).show();
-        });
-        $(".ecommerce img").click(function(){
-          $( ".ecommerce" ).hide();
-          $( ".business-systems" ).show();
-        });
-      });
+// :javascript
+//       $(document).ready(function(){
+//         $(".landing img").click(function(){
+//           $( ".landing" ).hide();
+//           $( ".ucd" ).show();
+//         });
+//         $(".ucd img").click(function(){
+//           $( ".ucd" ).hide();
+//           $( ".ecommerce" ).show();
+//         });
+//         $(".ecommerce img").click(function(){
+//           $( ".ecommerce" ).hide();
+//           $( ".business-systems" ).show();
+//         });
+//         $(".ecommerce img").click(function(){
+//           $( ".ecommerce" ).hide();
+//           $( ".business-systems" ).show();
+//         });
+//       });
 
-      :javascript
-	$(document).ready(function(){
-		location.reload();   
-	});
+//       :javascript
+// 	$(document).ready(function(){
+// 		location.reload();   
+// 	});
     
+
+// :javascript
+//       $(document).ready(function(){
+//         $(".landing img").click(function(){
+//           $( ".landing" ).hide();
+//           $( ".ucd h1, .ucd p, .ucd img" ).show();
+//           $( ".ucd video" ).animate({
+//             opacity: 1}, 1500 );
+//         });
+//         $(".ucd img").click(function(){
+//           $( ".ucd" ).hide();
+//           $( ".ecommerce h1, .ecommerce p, .ecommerce img" ).show();
+//           $( ".ecommerce video" ).animate({
+//             opacity: 1}, 1500 );
+//         });
+//         $(".ecommerce img").click(function(){
+//           $( ".ecommerce" ).hide();
+//           $( ".business-systems h1, .business-systems p, .business-systems img" ).show();
+//           $( ".business-systems video" ).animate({
+//             opacity: 1}, 1500 );
+//         });
+//       });
+
+// :javascript
+//   $(document).ready(function(){
+//     var x = 0;
+//     $("body").scroll(function(){
+//       if (x < 1) {
+//         $( ".landing" ).hide();
+//       }
+//     });
+//   });
+
+//   :javascript
+//       $(document).ready(function(){
+//         var x = 0;
+//         $("body").scroll(function(){
+//           x+= 1;
+//           alert('sdfakhlashf');
+//           if (x < 1) {
+//             alert('sdfakhlashf');
+//           }
+//         });
+//       });
